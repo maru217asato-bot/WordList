813e6c7c1d2b7d7e073357bfd49e5adae6e3c0e5
Mondifier <Mondifier>
2025-12-07 10:55:38 +0900

    Initial Commit

---
+++ WorldList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldList
+//
+//  Created by 丸岡　亜聡 on 2025/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldList/WorldListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldListApp.swift
+//  WorldList
+//
+//  Created by 丸岡　亜聡 on 2025/12/07.
+//
+
+import SwiftUI
+
+@main
+struct WorldListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
